2dffa3c75fb297c1df5cdd8095a6d26f64774eb3
Mayank Gupta <ce.mayank8@gmail.com>
2024-08-24 15:39:13 +0530

    Project Chess App: move queen

--- src/main/java/dev/mayank/piece/Queen.java
+++ src/main/java/dev/mayank/piece/Queen.java
@@ -13,4 +13,19 @@ public class Queen extends ChessPiece {
             this.setImage(getImage("/images/piece/b-queen"));
         }
     }
+
+    @Override
+    public boolean canMove(int targetRow, int targetCol) {
+        if (isWithinBoard(targetRow, targetCol) && !isSameSquare(targetRow, targetCol)) {
+            if (targetRow == getPrevRow() || targetCol == getPrevCol()) {   // horizontal or vertical step
+                if (isValidNextMove(targetRow, targetCol) && !isPieceOnStraightPath(targetRow, targetCol))
+                    return true;
+            }
+            if (Math.abs(targetRow - getPrevRow()) == Math.abs(targetCol - getPrevCol())) {  // diagonal step
+                if (isValidNextMove(targetRow, targetCol) && !isPieceOnDiagonalPath(targetRow, targetCol))
+                    return true;
+            }
+        }
+        return false;
+    }
 }
